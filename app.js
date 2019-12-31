e485ae44a9dc96109f2d28d66d3f9d4826363de9
Your Name <you@example.com>
2019-12-31 13:13:48 +0530

    Setting Up The FrontEnd and app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser({ extended: true }));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+});
+
+app.listen(3000, function() {
+  console.log("server is running on port 3000");
+});
